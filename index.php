276ae49c9064c48c11c5139d27d041f811e03587
khoursaaf <khoursa@gmail.com>
2018-11-19 10:11:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Traduire ce code avec des if et des else :</p>
+      <p><code>  echo ($isOk == false) ? 'c\'est pas bon !!!' : 'c\'est ok !!';</code></p>
+      <p>
+        <?php
+        $isOk = false;
+        if (!$isOk) {
+        echo 'c\'est pas bon !!!';
+        } else {
+          echo 'c\'est ok !!';
+        }
+         ?>
+      </p>
+    </body>
+</html>
