2984c775cf31ee9773341d4aa010b084813904ed
khoursaaf <khoursa@gmail.com>
2018-11-20 16:23:57 +0100

    correction

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Traduire ce code avec des if et des else :</p>
-      <p><code>  echo ($isOk == false) ? 'c\'est pas bon !!!' : 'c\'est ok !!';</code></p>
-      <p>
-        <?php
-        $isOk = false;
-        if (!$isOk) {
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Traduire ce code avec des if et des else :</p>
+    <p><code>  echo ($isOk == false) ? 'c\'est pas bon !!!' : 'c\'est ok !!';</code></p>
+    <p>
+      <?php
+      $isOk = false;
+      if ($isOk == false) {
         echo 'c\'est pas bon !!!';
-        } else {
-          echo 'c\'est ok !!';
-        }
-         ?>
-      </p>
-    </body>
+      } else {
+        echo 'c\'est ok !!';
+      }
+      ?>
+    </p>
+  </body>
 </html>
